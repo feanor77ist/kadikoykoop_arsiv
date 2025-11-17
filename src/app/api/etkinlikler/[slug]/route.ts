9a98195847be4f897ce329d93537931d06693aa5
feanor77ist <a.ardas@gmail.com>
2025-11-17 14:24:59 +0300

    api routes runtime ve dynamic eklendi

--- src/app/api/etkinlikler/[slug]/route.ts
+++ src/app/api/etkinlikler/[slug]/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from "next/server";
 import { readFile } from "fs/promises";
 import { join } from "path";
 
+export const runtime = 'nodejs';
+export const dynamic = 'force-dynamic';
+
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ slug: string }> }
